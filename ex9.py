42735f123a0ab125ecff0ceb9d1686437a68af59
Kelvin <kelvin.mozart@live.com>
2019-03-27 20:53:16 -0300

    Exercise 9

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#Exercise 9: Printing, Printing, Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print ("""
+There's something going om here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
